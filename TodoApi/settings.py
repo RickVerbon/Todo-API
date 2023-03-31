174e59726540eed5be830fe28cba1f3925395c36
Rick Verbon <rick89@gmail.com>
2023-03-31 08:30:37 +0200

    Allowed cors just * for testing

--- TodoApi/settings.py
+++ TodoApi/settings.py
@@ -128,9 +128,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:5173',
+CORS_ALLOWED_WHITELIST = [
+    '*',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TIME_ZONE = 'UTC'
 
